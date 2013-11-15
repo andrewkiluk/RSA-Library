17a41afe16eb71c1c73494e48c6c2f7d0cc93628
Andrew Kiluk <andrew.kiluk@gmail.com>
2013-11-15 14:12:32 -0500

    Still some issues with variable sizes.

--- rsa.c
+++ rsa.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
+#include <string.h>
+
 
 char buffer[1024];
 const int MAX_DIGITS = 50;
@@ -41,6 +43,22 @@ int64_t ExtEuclid(int64_t a, int64_t b)
    return y;
 }
 
+int rsa_modExp(int64_t b, int64_t e, int64_t m)
+{
+  int64_t remainder;
+  int64_t x = 1;
+
+  while (e != 0)
+  {
+    remainder = e % 2;
+    e = e/2;
+    if (remainder == 1){
+      x = (x * b) % m;
+      b = (b * b) % m;
+    }
+  }
+  return x;
+}
 
 // Calling this function will generate a public and private key and store them in the pointers
 // it is given. 
@@ -67,7 +85,6 @@ void rsa_gen_keys(struct public_key_class *pub, struct private_key_class *priv, 
   
   
   // choose random primes from the list, store them as p,q
-  srand(time(NULL));
 
   int64_t p = 0;
   int64_t q = 0;
@@ -77,6 +94,9 @@ void rsa_gen_keys(struct public_key_class *pub, struct private_key_class *priv, 
   char prime_buffer[MAX_DIGITS];
   int64_t max = 0;
   int64_t phi_max = 0;
+  
+  srand(time(NULL));
+  
   do{
     // a and b are the positions of p and q in the list
     int a =  (double)rand() * (prime_count+1) / (RAND_MAX+1.0);
@@ -105,7 +125,7 @@ void rsa_gen_keys(struct public_key_class *pub, struct private_key_class *priv, 
     max = p*q;
     phi_max = (p-1)*(q-1);
   }
-  while(gcd(phi_max, e) != 1);
+  while(!(p && q) || (p == q) || (gcd(phi_max, e) != 1));
  
   // Next, we need to choose a,b, so that a*max+b*e = gcd(max,e). We actually only need b
   // here, and in keeping with the usual notation of RSA we'll call it d. We'd also like 
@@ -115,6 +135,7 @@ void rsa_gen_keys(struct public_key_class *pub, struct private_key_class *priv, 
     d = d+phi_max;
   }
 
+  printf("primes are %lld and %lld\n",(long long)p, (long long )q);
   // We now store the public / private keys in the appropriate structs
   pub->modulus = max;
   pub->exponent = e;
@@ -133,10 +154,11 @@ int64_t *rsa_encrypt(const char *message, const unsigned long message_size,
      "Error: Heap allocation failed.\n");
     return NULL;
   }
-  int i = 0;
+  int64_t i = 0;
   for(i=0; i < message_size; i++){
-    encrypted[i] = pow(message[i],pub->exponent);
-  }
+    encrypted[i] = message[i];
+    encrypted[i] = rsa_modExp(encrypted[i], pub->exponent, pub->modulus);
+    }
   return encrypted;
 }
 
@@ -150,49 +172,29 @@ char *rsa_decrypt(const int64_t *message,
      "Error: message_size is not divisible by %d, so cannot be output of rsa_encrypt\n", (int)sizeof(int64_t));
      return NULL;
   }
-  char *decrypted = malloc(message_size*sizeof(int64_t));
-  if(decrypted == NULL){
+  // We allocate space to do the decryption (temp) and space for the output as a char array
+  // (decrypted)
+  char *decrypted = malloc(message_size*sizeof(char));
+  char *temp = malloc(message_size*sizeof(int64_t));
+  if((decrypted == NULL) || (temp == NULL)){
     fprintf(stderr,
      "Error: Heap allocation failed.\n");
     return NULL;
   }
-  int i = 0;
+  // Now we go through each 8-byte chunk and decrypt it.
+  int64_t i = 0;
   for(i=0; i < message_size; i++){
-    decrypted[i] = pow(message[i],priv->exponent);
+    temp[i] = message[i];
+    temp[i] = rsa_modExp(temp[i], priv->exponent, priv->modulus);
   }
+  // The result should be a number in the char range, which gives back the original byte.
+  // We put that into decrypted, then return.
+  for(i=0; i < message_size/8; i++){
+    decrypted[i] = temp[i];
+  }
+  free(temp);
   return decrypted;
 }
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-
-
-
-
-
-
-
-
-
-
 
 
 
--- test.c
+++ test.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include "rsa.h"
+#include <string.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv)
 {
@@ -8,15 +10,22 @@ int main(int argc, char **argv)
   rsa_gen_keys(pub, priv, PRIME_SOURCE_FILE);
   printf("Private Key:\n Modulus: %lld\n Exponent: %lld\n", (long long)priv->modulus, (long long) priv->exponent);
   printf("Public Key:\n Modulus: %lld\n Exponent: %lld\n", (long long)pub->modulus, (long long) pub->exponent);
-  char message[] = "abc123";
+  char message[] = "123abc";
  
   int64_t *encrypted = rsa_encrypt(message, sizeof(message), pub);
   char *decrypted = rsa_decrypt(encrypted, sizeof(encrypted), priv);
   
   int i;
-  for(i=0; i < sizeof(message); i++){
-    printf("%d\n", decrypted[i]);
+  printf("Encrypted:\n");
+  for(i=0; i < strlen(message); i++){
+    printf("%lld\n", (long long)encrypted[i]);
+  }  
+  printf("Decrypted:\n");
+  for(i=0; i < strlen(message); i++){
+    printf("%lld\n", (long long)decrypted[i]);
   }  
   printf("\n");
+  free(encrypted);
+  free(decrypted);
   return 0;
 }
